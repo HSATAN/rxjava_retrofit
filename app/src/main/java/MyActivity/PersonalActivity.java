1d25da80f033e860e5bf219490302843c1ae3062
huangkaijie <huangkaijie@huangkaijiedeiMac.local>
2017-10-23 16:31:20 +0800

    item 修改

---
+++ app/src/main/java/MyActivity/PersonalActivity.java
@@ -0,0 +1,18 @@
+package Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by huangkaijie on 2017/10/23.
+ */
+
+public class PersonalActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+
+    }
+}
