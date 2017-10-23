ad9288069a596f3d6d0c488faced8b447191129f
huangkaijie <huangkaijie@huangkaijiedeiMac.local>
2017-10-23 14:12:17 +0800

    添加recyclerview

---
+++ app/src/main/java/DataAdapter/RecylerViewAdapter.java
@@ -0,0 +1,8 @@
+package DataAdapter;
+
+/**
+ * Created by huangkaijie on 2017/10/23.
+ */
+
+public class RecylerViewAdapter {
+}
---
+++ app/src/main/java/com/example/huangkaijie/myapplication/RecylerActivity.java
@@ -0,0 +1,10 @@
+package com.example.huangkaijie.myapplication;
+
+import android.app.Activity;
+
+/**
+ * Created by huangkaijie on 2017/10/23.
+ */
+
+public class RecylerActivity extends Activity {
+}
