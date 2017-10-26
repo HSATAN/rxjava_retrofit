d5b841c755e0d871c07a96f8081ef301de984591
huangkaijie <huangkaijie@huangkaijiedeiMac.local>
2017-10-26 17:45:45 +0800

    fix

---
+++ app/src/main/java/DataAdapter/IndexAdapter.java
@@ -0,0 +1,10 @@
+package DataAdapter;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by huangkaijie on 2017/10/26.
+ */
+
+public class IndexAdapter extends FragmentPagerAdapter {
+}
